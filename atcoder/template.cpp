0cb30cc57cb8e63969699924626c368a412647dc
t-akzw <45358756+t-akzw@users.noreply.github.com>
2021-02-07 02:36:44 +0900

    template

---
+++ atcoder/template.cpp
@@ -0,0 +1,42 @@
+#include <bits/stdc++.h>
+#define rep(i,n) for (int i = 0; i < (n); ++i)
+using namespace std;
+typedef long long ll;
+typedef pair<int,int> P;
+ 
+const int mod = 1000000007;
+struct mint {
+  ll x;
+  mint(ll x=0):x(x%mod){}
+  mint& operator+=(const mint a) {
+    if ((x += a.x) >= mod) x -= mod;
+    return *this;
+  }
+  mint& operator-=(const mint a) {
+    if ((x += mod-a.x) >= mod) x -= mod;
+    return *this;
+  }
+  mint& operator*=(const mint a) {
+    (x *= a.x) %= mod;
+    return *this;
+  }
+  mint operator+(const mint a) const {
+    mint res(*this);
+    return res+=a;
+  }
+  mint operator-(const mint a) const {
+    mint res(*this);
+    return res-=a;
+  }
+  mint operator*(const mint a) const {
+    mint res(*this);
+    return res*=a;
+  }
+};
+ 
+int main() {
+  int n, k;
+  cin >> n >> k;
+  cout << n << k << endl;
+  return 0;
+}
