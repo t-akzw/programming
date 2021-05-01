db2e45fe7ae57c344a7469ad990181381e036cda
t-akzw <45358756+t-akzw@users.noreply.github.com>
2021-05-02 02:08:25 +0900

    abc004ab

--- atcoder/abc004/exec.sh
+++ atcoder/abc004/exec.sh
@@ -1,16 +1,23 @@
 #!/bin/sh
 
+EXAMPATH='./examples'
+OUTPATH='./outputs'
+
+mkdir -p $OUTPATH
+
+[[ $1 == '' ]] && echo "please input (ex.a-f)" && exit 1
+
 execfile="./${1}.out"
 
-filenum=$(ls -1 input_example_${1}_* |wc -l |sed "s/ //g")
+filenum=$(ls -1 ${EXAMPATH}/i_${1}_* |wc -l |sed "s/ //g")
 
 echo "expected, output"
 for i in `seq 1 ${filenum}`
 do
   echo "example ${i} ----"
-  input_example_filename="input_example_${1}_${i}.txt"
-  output_example_filename="output_example_${1}_${i}.txt"
-  output_filename="output_${1}_${i}.txt"
+  input_example_filename="${EXAMPATH}/i_${1}_${i}"
+  output_example_filename="${EXAMPATH}/o_${1}_${i}"
+  output_filename="${OUTPATH}/o_${1}_${i}"
   $execfile < $input_example_filename > $output_filename
   diff -y --suppress-common-lines $output_example_filename $output_filename
 done
--- atcoder/abc004/getexam.sh
+++ atcoder/abc004/getexam.sh
@@ -1,15 +1,19 @@
 #!/bin/sh
 
 AUTOMATION_PATH='../automation'
+EXAMPATH='./examples'
 
-curl https://atcoder.jp/contests/${1}/tasks/${1}_${2} > ${AUTOMATION_PATH}/app/atcoder.html
+[[ $1 == '' ]] && echo "please input (ex.abc001)" && exit 1
+[[ $2 == '' ]] && echo "please input (ex.a-f)" && exit 1
+[[ $3 == 'curl' ]] && curl https://atcoder.jp/contests/${1}/tasks/${1}_${2} > ${AUTOMATION_PATH}/app/atcoder.html
 
 ./scraping.sh
 
-mv ${AUTOMATION_PATH}/app/*_example_*.txt .
+mkdir -p $EXAMPATH
+mv ${AUTOMATION_PATH}/app/*_example_*.txt $EXAMPATH
 
-for i in $(ls -1 *_example_*.txt)
+for i in $(ls -1 $EXAMPATH/*_example_*.txt)
 do
-  name=$(echo $i |sed "s/example_/example_${2}_/g")
-  mv $i $name
+  name=$(echo $i |sed "s/.*\/\([i|o]\).*_example_\([0-9]\)\.txt/\1_${2}_\2/g")
+  mv $i $EXAMPATH/$name
 done
