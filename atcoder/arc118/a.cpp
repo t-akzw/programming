42049c746884e0f99bdb82f9d1939953c5f5e180
t-akzw <45358756+t-akzw@users.noreply.github.com>
2021-05-10 00:32:19 +0900

    arc118a

---
+++ atcoder/arc118/a.cpp
@@ -0,0 +1,86 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+using ll = long long;
+using P = pair<ll, ll>;
+using V = vector<ll>;
+using VV = vector<V>;
+using M = map<ll, ll>;
+using Q = queue<ll>;
+using PQ = priority_queue<ll>;
+using ST = stack<ll>;
+using Graph = vector< vector<ll> >;
+
+#define rep(i, n)     for(ll i=0;i<(ll)(n);i++)
+#define rep2(i, m, n) for(ll i=m;i<(ll)(n);i++)
+#define rrep(i, n, m) for(ll i=n;i>=(ll)(m);i--)
+#define rbfor(v, vec) for(auto const& v : vec)
+#define debug(var)  do{std::cout << #var << " : ";view(var);}while(0)
+
+const ll MOD = 1000000007;
+const ll INF = 1000000000000000000L;
+const int INFI = 1000000000;
+template<typename T> bool chmax(T &a, const T &b) {if (a < b){a = b;return true;}else{return false;}}
+template<typename T> bool chmin(T &a, const T &b) {if (a > b){a = b;return true;}else{return false;}}
+template<typename T> void view(T e){std::cout << e << std::endl;}
+template<typename T> void view(const std::vector<T>& v){for(const auto& e : v){ std::cout << e << " "; } std::cout << std::endl;}
+template<typename T> void view(const std::vector<std::vector<T> >& vv){ for(const auto& v : vv){ view(v); } }
+
+// for dfs
+string s[505];
+bool used[505][505];
+int d[505][505];
+
+const int di[] = {-1,0,1,0}, dj[] = {0,-1,0,1};
+
+void sample_solve();
+int gcd(ll m, ll n);
+bool dfs(int i, int j);
+void bfs();
+
+// =================================
+// Main Logic
+// =================================
+void solve() {
+  double t;
+  int n;
+  cin >> t >> n;
+  ll tmp = (ll)(100.0*n)/t;
+  //debug(tmp);
+  if ((int)t*n%50 == 0) tmp -=1;
+  if(tmp%10==0) tmp -=1;
+  //debug(tmp);
+  ll ans = (ll)floor(tmp + (double)(t*tmp)/100.0) + 1;
+  //debug((ll)floor(tmp-1 + (double)(t*(tmp-1))/100.0));
+  //debug((ll)floor(tmp + (double)(t*tmp)/100.0));
+  //debug((ll)floor(tmp+1 + (double)(t*(tmp+1))/100.0));
+  cout << ans << endl;
+}
+// =================================
+
+int main() {
+  std::cin.tie(nullptr);
+  std::ios_base::sync_with_stdio(false);
+  std::cout << std::fixed << std::setprecision(15);
+  solve();
+  return 0;
+}
+
+// ---------------
+// https://atcoder.jp/contests/apg4b/tasks_print
+// ---------------
+
+typedef tuple<string, int ,int> TUPLE;
+bool comp(TUPLE lhs, TUPLE rhs) {
+  if (get<0>(lhs) == get<0>(rhs)) {
+    return get<1>(lhs) > get<1>(rhs);
+  } else {
+    return get<0>(lhs) < get<0>(rhs);
+  }
+}
+
+int gcd(ll m, ll n) {
+  if (n == 0) return m;
+  return gcd(n, m%n);
+}
